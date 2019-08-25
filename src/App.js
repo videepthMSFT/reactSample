6e189051ce313f9a0acaeb3db303717de112dbe4
Vishnu Naga Praveen Deepthimahanthi <videepth@microsoft.com>
2019-08-26 01:09:47 +0530

    container changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import HomePage from "./component/pages/HomePage";
 import LoginPage from "./component/pages/LoginPage";
 
 const App = () =>
-    <div>
+    <div className="ui container">
         <Route path="/" exact component={HomePage} />
         <Route path="/login" exact component={LoginPage}/>
     </div>;
