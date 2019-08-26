3f403a8a79cc1649f1c401dbe21a993d72b73296
Vishnu Naga Praveen Deepthimahanthi <videepth@microsoft.com>
2019-08-26 10:51:20 +0530

    Adding Redux

--- src/index.js
+++ src/index.js
@@ -4,10 +4,22 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import "semantic-ui-css/semantic.min.css";
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import rootReducer from './rootReducer';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
     <BrowserRouter>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </BrowserRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
